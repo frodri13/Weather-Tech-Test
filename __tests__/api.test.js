d9c2d7cccaf9e4a0543454d9845a1c57b3063713
Fabio <alessf13@gmail.com>
2021-10-26 23:31:22 +0200

    test: testing added for status of api and temperature

---
+++ __tests__/api.test.js
@@ -0,0 +1,21 @@
+const frisby = require("frisby");
+const Joi = frisby.Joi;
+const city = 2643743;
+const key = "9d50450a48809637b4862bdcb125927d";
+const url = `http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${key}&units=metric`;
+
+it("should return a status of 200", function () {
+  return frisby.get(url).expect("status", 200);
+});
+
+it("should have London as a city name", function () {
+  return frisby.get(url).expect("json", {
+    name: "London",
+  });
+});
+
+it("should have a temperature", function () {
+  return frisby.get(url).expect("jsonTypes", "main", {
+    temp: Joi.number(),
+  });
+});
--- interface.js
+++ interface.js
@@ -1,6 +1,17 @@
+let temperature = document.querySelector("#current-temperature");
+
+document.querySelector("#select-city").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const city = document.querySelector("#current-city").value;
+
+  if (city === "London") {
+    displayWeather(2643743);
+  }
+});
+
 const displayWeather = (city) => {
-  const api = "9d50450a48809637b4862bdcb125927d";
-  const url = `http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${api}&units=metric`;
+  const key = "9d50450a48809637b4862bdcb125927d";
+  const url = `http://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${key}&units=metric`;
 
   fetch(url)
     .then((response) => {
@@ -8,8 +19,6 @@ const displayWeather = (city) => {
     })
     .then((data) => {
       console.log(data);
-      document.querySelector("#current-temperature").innerText = data.main.temp;
+      temperature.innerText = data.main.temp;
     });
 };
-
-displayWeather(2643743);
